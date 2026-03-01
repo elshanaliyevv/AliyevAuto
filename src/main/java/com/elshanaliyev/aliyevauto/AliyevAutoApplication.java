62059ba12a021c40ab4f9b1675814a4f9084cf93
aliyev <elsnl63@gmail.com>
2026-03-01 16:35:53 +0400

    Initial commit

---
+++ src/main/java/com/elshanaliyev/aliyevauto/AliyevAutoApplication.java
@@ -0,0 +1,13 @@
+package com.elshanaliyev.aliyevauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliyevAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliyevAutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elshanaliyev/aliyevauto/AliyevAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elshanaliyev.aliyevauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliyevAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
